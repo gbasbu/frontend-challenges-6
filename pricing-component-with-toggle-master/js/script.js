9d1a77faf601717ea5ad09ba1d49e608fd70a592
gbasbu <gorkembasbu@gmail.com>
2021-01-20 20:14:03 +0300

    fixed html issues

--- pricing-component-with-toggle-master/js/script.js
+++ pricing-component-with-toggle-master/js/script.js
@@ -1,22 +1,22 @@
 function myFunction() {
     var basicPrice = document.getElementById('basicPrice')
-    if(basicPrice.innerHTML != '$19.99'){
-        basicPrice.innerHTML = '$19.99'
-    }else{
+    if(basicPrice.innerHTML != '$199.99'){
         basicPrice.innerHTML = '$199.99'
+    }else{
+        basicPrice.innerHTML = '$19.99'
     }
 
     var proPrice = document.getElementById('proPrice')
-    if(proPrice.innerHTML != '$24.99'){
-        proPrice.innerHTML = '$24.99'
-    }else{
+    if(proPrice.innerHTML != '$249.99'){
         proPrice.innerHTML = '$249.99'
+    }else{
+        proPrice.innerHTML = '$24.99'
     }
 
     var masterPrice = document.getElementById('masterPrice')
-    if(masterPrice.innerHTML != '$39.99'){
-        masterPrice.innerHTML = '$39.99'
-    }else{
+    if(masterPrice.innerHTML != '$399.99'){
         masterPrice.innerHTML = '$399.99'
+    }else{
+        masterPrice.innerHTML = '$39.99'
     }
 }
